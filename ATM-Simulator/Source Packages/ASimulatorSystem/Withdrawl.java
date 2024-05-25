6ac88c8529abb0e9075bfcedede4a8511fbebf58
Baheer Noori <96612910+NarutBaheer@users.noreply.github.com>
2024-05-25 21:58:34 +0430

    Update Withdrawl.java

--- ATM-Simulator/Source Packages/ASimulatorSystem/Withdrawl.java
+++ ATM-Simulator/Source Packages/ASimulatorSystem/Withdrawl.java
@@ -15,13 +15,13 @@ public class Withdrawl extends JFrame implements ActionListener{
     Withdrawl(String pin){
         this.pin = pin;
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("ASimulatorSystem/icons/atm.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(1000, 1180, Image.SCALE_DEFAULT);
+        Image i2 = i1.getImage().getScaledInstance(1000, 700, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel l3 = new JLabel(i3);
-        l3.setBounds(0, 0, 960, 1080);
+        l3.setBounds(0, 0, 960, 700);
         add(l3);
         
-        l1 = new JLabel("MAXIMUM WITHDRAWAL IS RS.10,000");
+        l1 = new JLabel("MAXIMUM WITHDRAWAL IS $ 10,000");
         l1.setForeground(Color.WHITE);
         l1.setFont(new Font("System", Font.BOLD, 16));
         
@@ -37,26 +37,26 @@ public class Withdrawl extends JFrame implements ActionListener{
         
         setLayout(null);
         
-        l1.setBounds(190,350,400,20);
+        l1.setBounds(195,150,400,20);
         l3.add(l1);
         
-        l2.setBounds(190,400,400,20);
+        l2.setBounds(190,195,400,20);
         l3.add(l2);
         
-        t1.setBounds(190,450,330,30);
+        t1.setBounds(190,200,330,30);
         l3.add(t1);
         
-        b1.setBounds(390,588,150,35);
+        b1.setBounds(270,320,150,35);
         l3.add(b1);
         
-        b2.setBounds(390,633,150,35);
+        b2.setBounds(270,360,150,35);
         l3.add(b2);
         
         b1.addActionListener(this);
         b2.addActionListener(this);
         
-        setSize(960,1080);
-        setLocation(500,0);
+        setSize(960,800);
+        setLocationRelativeTo(null);
         setUndecorated(true);
         setVisible(true);
     }
@@ -75,7 +75,7 @@ public class Withdrawl extends JFrame implements ActionListener{
                     ResultSet rs = c1.s.executeQuery("select * from bank where pin = '"+pin+"'");
                     int balance = 0;
                     while(rs.next()){
-                       if(rs.getString("mode").equals("Deposit")){
+                       if(rs.getString("type").equals("Deposit")){
                            balance += Integer.parseInt(rs.getString("amount"));
                        }else{
                            balance -= Integer.parseInt(rs.getString("amount"));
