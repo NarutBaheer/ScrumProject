7db461322f0d8a21efdd847841e4960531d745f1
Baheer Noori <96612910+NarutBaheer@users.noreply.github.com>
2024-05-25 21:49:41 +0430

    Update BalanceEquiry.java

--- ATM-Simulator/Source Packages/ASimulatorSystem/BalanceEquiry.java
+++ ATM-Simulator/Source Packages/ASimulatorSystem/BalanceEquiry.java
@@ -17,10 +17,10 @@ class BalanceEnquiry extends JFrame implements ActionListener {
         this.pin = pin;
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("ASimulatorSystem/icons/atm.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(1000, 1180, Image.SCALE_DEFAULT);
+        Image i2 = i1.getImage().getScaledInstance(1000, 700, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel l3 = new JLabel(i3);
-        l3.setBounds(0, 0, 960, 1080);
+        l3.setBounds(0, 0, 960, 700);
         add(l3);
 
         l1 = new JLabel();
@@ -31,17 +31,17 @@ class BalanceEnquiry extends JFrame implements ActionListener {
 
         setLayout(null);
 
-        l1.setBounds(190, 350, 400, 35);
+        l1.setBounds(165, 210, 400, 35);
         l3.add(l1);
 
-        b1.setBounds(390, 633, 150, 35);
+        b1.setBounds(270, 360, 150, 35);
         l3.add(b1);
         int balance = 0;
         try{
             Conn c1 = new Conn();
             ResultSet rs = c1.s.executeQuery("select * from bank where pin = '"+pin+"'");
             while (rs.next()) {
-                if (rs.getString("mode").equals("Deposit")) {
+                if (rs.getString("type").equals("Deposit")) {
                     balance += Integer.parseInt(rs.getString("amount"));
                 } else {
                     balance -= Integer.parseInt(rs.getString("amount"));
@@ -49,13 +49,13 @@ class BalanceEnquiry extends JFrame implements ActionListener {
             }
         }catch(Exception e){}
         
-        l1.setText("Your Current Account Balance is Rs "+balance);
+        l1.setText("Your Current Account Balance is $ "+balance);
 
         b1.addActionListener(this);
 
-        setSize(960, 1080);
+        setSize(960, 700);
         setUndecorated(true);
-        setLocation(500, 0);
+        setLocationRelativeTo(null);
         setVisible(true);
     }
 
@@ -68,3 +68,4 @@ class BalanceEnquiry extends JFrame implements ActionListener {
         new BalanceEnquiry("").setVisible(true);
     }
 }
+
